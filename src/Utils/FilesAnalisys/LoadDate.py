e56cc6f1b77b91ed3b59198960db2a9573d0cbf4
Itamar Efrati <Itamar.Efrati@teva.co.il>
2022-01-12 12:54:39 +0200

    Removed files utils from track

--- src/Utils/FilesAnalisys/LoadDate.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-
-def read_panss_scores():
-    return pd.read_csv(r"data\Preprocessed\panss_scores.csv", index_col=['Id', 'Date'], parse_dates=['Date'])
-
-
-def read_lab_results():
-    return pd.read_csv(r"data\Preprocessed\lab_results.csv", index_col=['Id', 'Date'], parse_dates=['Date'])
